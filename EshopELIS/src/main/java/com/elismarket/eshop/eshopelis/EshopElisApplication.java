2c6efe7ac40d81d950fc5ebed1daa8634b5b7b5c
GamesDeny <montrano.francesco@gmail.com>
2021-03-16 11:35:43 +0100

    Added controllers and started creating the logic

--- EshopELIS/src/main/java/com/elismarket/eshop/eshopelis/EshopElisApplication.java
+++ EshopELIS/src/main/java/com/elismarket/eshop/eshopelis/EshopElisApplication.java
@@ -2,8 +2,12 @@ package com.elismarket.eshop.eshopelis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.elismarket.eshop.*"})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.elismarket.eshop.businesslogic"})
+@EntityScan(basePackages = {"com.elismarket.eshop.model"})
 public class EshopElisApplication {
 
     public static void main(String[] args) {
