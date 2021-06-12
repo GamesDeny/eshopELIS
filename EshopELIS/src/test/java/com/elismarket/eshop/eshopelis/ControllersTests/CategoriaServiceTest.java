c0eefb8b6211dec82fafc878a19c2215b50e104a
GamesDeny <montrano.francesco@gmail.com>
2021-06-12 14:14:41 +0200

    Removed linking methods between classes

--- EshopELIS/src/test/java/com/elismarket/eshop/eshopelis/ControllersTests/CategoriaServiceTest.java
+++ EshopELIS/src/test/java/com/elismarket/eshop/eshopelis/ControllersTests/CategoriaServiceTest.java
@@ -4,13 +4,11 @@ import com.elismarket.eshop.eshopelis.dto.CategoriaDTO;
 import com.elismarket.eshop.eshopelis.exception.CategoriaException;
 import com.elismarket.eshop.eshopelis.model.Categoria;
 import com.elismarket.eshop.eshopelis.repository.CategoriaCrud;
-
 import com.elismarket.eshop.eshopelis.service.interfaces.CategoriaService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,24 +28,25 @@ public class CategoriaServiceTest {
     CategoriaCrud categoriaCrud;
 
     @BeforeEach
-    public void init(){deleteDb();};
+    public void init() {
+        deleteDb();
+    }
 
 
     @Test
     public void testAddCategoria() {
         //inizializzo una categoria di test e la aggiungo nel db
-        Categoria categoria = new Categoria();
-        categoria.setNome("pizza");
-        categoria.setId(1l);
-        List<CategoriaDTO> categoriaDTO=  new ArrayList<>();
-        categoriaDTO.add(Categoria.to(categoria));
+        CategoriaDTO categoria = new CategoriaDTO();
+        categoria.nome = "Altro";
 
+        categoria = categoriaService.addCategoria(categoria);
         //testo che il metodo funzioni
-        assertNotNull( categoriaService.addCategoria(categoriaDTO.get(0)));
-        assertTrue(categoriaService.getById(categoriaService.addCategoria(Categoria.to(categoria)).id) instanceof CategoriaDTO);
+        assertNotNull(categoria);
+        assertNotNull(categoriaService.getById(categoria.id));
+        assertEquals("Altro", (categoriaService.getById(categoria.id).nome));
 
         //testo che lanci l'eccezione con un oggetto vuoto
-        assertThrows(CategoriaException.class, ()->{
+        assertThrows(CategoriaException.class, () -> {
             categoriaService.addCategoria(null);
         });
     }
@@ -55,34 +54,40 @@ public class CategoriaServiceTest {
     @Test
     public void testUpdateCategoria() {
         //inizializzo una categoria di test e la aggiungo nel db
-        Categoria categoria = new Categoria();
-        categoria.setNome("pizza");
-        categoria.setId(1l);
-        //prendo dal db l'id della categoria generato
-        List<CategoriaDTO> categoriaDTO=  new ArrayList<>();
-        categoriaDTO.add(Categoria.to(categoria));
-        //prendo dal db l'id della categoria generato
+        CategoriaDTO categoria = new CategoriaDTO();
+        categoria.nome = "Altro";
 
+        categoria = categoriaService.addCategoria(categoria);
+        assertNotNull(categoria.id);
+        final Long id = categoria.id;
 
-        //testo che ritorni un oggetto
-        assertNotNull(categoriaService.updateCategoria(1l,Categoria.to(categoria)));
+        CategoriaDTO updatedCat = new CategoriaDTO();
+        updatedCat.nome = "Pulizia";
 
         //testo che lanci l'eccezione in caso non gli passi nulla
-        assertThrows(CategoriaException.class, ()->{
-            categoriaService.updateCategoria(null,null);
+        assertThrows(CategoriaException.class, () -> {
+            categoriaService.updateCategoria(null, null);
         });
 
-        //testo che lanci l'eccezione in caso non gli passi un id non presente
-        assertThrows(CategoriaException.class, ()->{
-            categoriaService.updateCategoria(2L,categoriaDTO.get(0));
+        //testo che lanci l'eccezione in caso non gli passi il DTO aggiornato
+        assertThrows(CategoriaException.class, () -> {
+            categoriaService.updateCategoria(id, null);
         });
+
+        //testo che lanci l'eccezione in caso non gli passi l'id
+        assertThrows(CategoriaException.class, () -> {
+            categoriaService.updateCategoria(null, updatedCat);
+        });
+
+        //testo che lanci l'eccezione in caso gli passi un id non presente
+        assertThrows(CategoriaException.class, () -> {
+            categoriaService.updateCategoria(2L, updatedCat);
+        });
+
+
         //testo che lanci l'eccezione in caso non gli passi un oggetto nullo come categoriaDTO
-        assertThrows(CategoriaException.class, ()->{
-            categoriaService.updateCategoria(1l,null);
-        });
-
-
-
+        assertNotNull(categoriaService.updateCategoria(1L, updatedCat));
+        assertEquals("Pulizia", categoriaService.getById(id).nome);
 
     }
 
@@ -90,7 +95,7 @@ public class CategoriaServiceTest {
     public void testDeleteCategoria() {
         //inizializzo una categoria di test e la aggiungo nel db
         Categoria categoria = new Categoria();
-        categoria.setNome("pizza");
+        categoria.setNome("Altro");
         long id = categoriaService.addCategoria(Categoria.to(categoria)).id;
         //prendo dal db l'id della categoria generato
 
@@ -114,7 +119,7 @@ public class CategoriaServiceTest {
     public void testGetAllCategoria() {
         //inizializzo una categoria di test e la aggiungo nel db
         Categoria categoria = new Categoria();
-        categoria.setNome("pizza");
+        categoria.setNome("Altro");
         List<CategoriaDTO> categoriaDTO=  new ArrayList<>();
         List<Categoria> categorie =new ArrayList<>();
         categorie.add(categoria);
@@ -139,7 +144,7 @@ public class CategoriaServiceTest {
     public void testGetCategoria() {
         //inizializzo una categoria di test e la aggiungo nel db
         Categoria categoria = new Categoria();
-        categoria.setNome("pizza");
+        categoria.setNome("Altro");
         categoriaService.addCategoria(Categoria.to(categoria));
         //prendo dal db l'id della categoria generato
         long id= categoriaService.getAll().get(0).id;
